e00210c0f65ba08cb6d421a0828aecd7dfd41b00
dantemitchell <145046281+dantemitchell@users.noreply.github.com>
2024-04-04 11:18:52 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 window.addEventListener('load', function() {
-    // Generate a random color
-    var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
 
-    // Change background color to the random color
-    document.body.style.backgroundColor = randomColor;
-
-    // Show a popup alert with a welcome message
     alert('Welcome to Dante\'s Guide to Everything!');
 });
